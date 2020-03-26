0b02cf6ce539ba894448775bd8848c4a011c46cf
rkritchat <rkritchat@gmail.com>
2020-03-26 17:54:29 +0700

    clean main

--- main.go
+++ main.go
@@ -14,11 +14,15 @@ func main() {
 	defer db.Close()
 
 	h := service.NewHandle(&db)
+	r := addHandler(h)
+	panic(http.ListenAndServe(":8080", r))
+}
 
+func addHandler(h *service.Handlers) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc(path.RegisterPath, h.Register).Methods(http.MethodPost)
 	r.HandleFunc(path.UpdatePath, h.Update).Methods(http.MethodPost)
 	r.HandleFunc(path.InquiryPath, h.Inquiry).Methods(http.MethodGet)
 	r.HandleFunc(path.InquiryAllPath, h.InquiryAll).Methods(http.MethodGet)
-	http.ListenAndServe(":8080", r)
+	return r
 }
